3240289bc8bd79b6c545947373ffa06371c95d9c
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2025-09-12 09:57:25 +0500

    fix(preprocessor): 🐛 Fix and refactor toolchain definitions

--- gofra/cli/definitions.py
+++ gofra/cli/definitions.py
@@ -5,19 +5,7 @@ def construct_propagated_toolchain_definitions(
     *,
     target: Target,
 ) -> dict[str, str]:
-    toolchain_definitions = {}
-    match target.operating_system:
-        case "Darwin":
-            toolchain_definitions = {
-                "OS_POSIX": "1",
-                "OS_DARWIN": "1",
-                "OS_MACOS": "1",
-            }
-
-        case "Linux":
-            toolchain_definitions = {
-                "OS_POSIX": "1",
-                "OS_LINUX": "1",
-            }
-
-    return toolchain_definitions
+    return {
+        f"ARCH_{target.architecture.upper()}": "1",
+        f"OS_{target.operating_system.upper()}": "1",
+    }
