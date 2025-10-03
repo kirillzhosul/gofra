14dcdcde790a512628b0f93e876a50c8226ebd4f
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2025-10-03 16:48:02 +0500

    fix(types): 🐛 Fix obscured 8 bytes character by default

--- gofra/types/primitive/character.py
+++ gofra/types/primitive/character.py
@@ -4,7 +4,7 @@ from gofra.types._base import PrimitiveType
 class CharType(PrimitiveType):
     """Character type as an almost as-is alias to an integer."""
 
-    size_in_bytes = 8  # TODO(@kirillzhosul): Checkout usage without whole 8 bytes
+    size_in_bytes = 1
 
     def __repr__(self) -> str:
         return "Character"
