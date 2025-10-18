c6c4807e30e4666bcf983d23ba66de098e630f43
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2025-10-18 14:26:43 +0500

    fix: 🐛 Fix Windows target host inference

--- gofra/targets/infer_host.py
+++ gofra/targets/infer_host.py
@@ -5,12 +5,12 @@ from gofra.targets.target import Target
 
 def infer_host_target() -> Target | None:
     """Try to infer target from current system."""
-    assert system() in ["Darwin", "Linux"]
-
     match system():
         case "Darwin":
             return Target.from_triplet("arm64-apple-darwin")
         case "Linux":
             return Target.from_triplet("amd64-unknown-linux")
+        case "Windows":
+            return Target.from_triplet("amd64-unknown-windows")
         case _:
             return None
