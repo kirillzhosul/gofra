776667fe40e96a5e8181489a90304bb6590a78e4
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2026-01-13 09:50:15 +0400

    fix: 🚑️ Fix corrupted space in source code [skip ci]

--- libgofra/types/generics.py
+++ libgofra/types/generics.py
@@ -111,7 +111,7 @@ def apply_generic_type_into_concrete(
                 )  # Always concrete defined at generic definition
             else:
                 # Generic VALUE param from application
-                concrete_elements_c ount = type_parameters[generic.element_count.name]
+                concrete_elements_count = type_parameters[generic.element_count.name]
                 if not isinstance(concrete_elements_count, int):
                     msg = f"Parameter of generic with typename '{generic.element_count.name}' has '{generic.element_count.kind.name}' kind but got an 'VALUE PARAM', cannot apply generic into concrete type \n[error-location-cannot-be-propagated]"
                     raise TypeError(msg)
