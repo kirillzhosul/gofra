f86adbfa3638954fbb4169f22b41787e0d013b50
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2026-01-18 23:34:22 +0400

    feat(cli): ✨ Version goal display assembler driver used [skip ci]

--- gofra/cli/goals/version.py
+++ gofra/cli/goals/version.py
@@ -3,6 +3,7 @@ from platform import platform, python_implementation, python_version
 from typing import NoReturn
 
 from gofra.cli.parser.arguments import CLIArguments
+from libgofra.assembler.drivers._get_assembler_driver import get_assembler_driver
 from libgofra.feature_flags import (
     FEATURE_ALLOW_FPU,
     FEATURE_ALLOW_MODULES,
@@ -18,15 +19,23 @@ def cli_perform_version_goal(args: CLIArguments) -> NoReturn:
         .removesuffix("_command")
     )
 
+    assembler_driver = get_assembler_driver(args.target)
+    assembler_driver_name = assembler_driver.name if assembler_driver else "No suitable"
+
     print("[Gofra toolchain]")
     print("Toolchain target (may be unavailable on host machine):")
     print(f"\tTriplet: {args.target.triplet}")
     print(f"\tArchitecture: {args.target.architecture}")
     print(f"\tOS: {args.target.operating_system}")
-    print(f"\t(linker command composer: {linker_composer_backend})")
+
+    print("Tools and steps:")
+    print(f"\tLinker backend: {linker_composer_backend}")
+    print(f"\tAssembler driver: {assembler_driver_name}")
+
     print("Host machine:")
     print(f"\tPlatform: {platform()}")
     print(f"\tPython: {python_implementation()} {python_version()}")
+
     print("Features:")
     print(f"\tFEATURE_ALLOW_FPU = {FEATURE_ALLOW_FPU}")
     print(f"\tFEATURE_ALLOW_MODULES = {FEATURE_ALLOW_MODULES}")
