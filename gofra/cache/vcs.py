159f6c2b4312c1652810686b8225ebd9ff203e4a
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2026-05-31 04:51:19 +0400

    fix: 🐛 Fix cache gitignore file cannot be created due to permissions error

--- gofra/cache/vcs.py
+++ gofra/cache/vcs.py
@@ -1,9 +1,16 @@
 from pathlib import Path
 
 
-def create_cache_gitignore(path: Path) -> None:
+def create_cache_gitignore(path: Path, *, strict_must_perform: bool = False) -> None:
     """Create .gitignore file for Git VCS to not include cache into VCS."""
-    with (path / ".gitignore").open("w") as f:
-        f.write("# Internally created by Gofra toolchain\n")
-        f.write("# Do not include this newly generated build cache into git VCS\n")
-        f.write("*\n")
+    gitignore = path / ".gitignore"
+
+    content = """# Internally created by Gofra toolchain\n
+# Do not include this newly generated build cache into git VCS\n
+*\n"""
+
+    try:
+        gitignore.write_text(content)
+    except PermissionError:
+        if strict_must_perform:
+            raise
